223ad0c1260adb972a148e131ac0c5f26c0514af
nikolakosturski <nkosturski6@gmail.com>
2024-11-14 18:23:21 +0100

    first commit

---
+++ InterviewTask/Polimorfizam2.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewTask2
+{
+
+    public class Animall
+    {
+
+
+
+        public virtual void OldMcDonaldd(string ime, string zvuk) { }
+
+    }
+
+    class Cow : Animall
+    {
+
+
+
+        public override void OldMcDonaldd(string ime, string zvuk)
+        {
+
+            Console.WriteLine("Old MacDonald had a farm, E I E I O,\n" +
+                $"And on his farm, he had a {ime}, E I E I O. \n" +
+                $"With a {zvuk},{zvuk} here and a {zvuk},{zvuk} there,\n" +
+                $"Here a {zvuk}, there a {zvuk}, everywhere a {zvuk} {zvuk}.\n" +
+                $"Old MacDonald had a farm, E I E I O.\n");
+        }
+
+
+    }
+
+    class Duck : Animall
+    {
+
+        public override void OldMcDonaldd(string ime, string zvuk)
+        {
+
+            Console.WriteLine("Old MacDonald had a farm, E I E I O,\n" +
+                $"And on his farm, he had a {ime}, E I E I O. \n" +
+                $"With a {zvuk},{zvuk} here and a {zvuk},{zvuk} there,\n" +
+                $"Here a {zvuk}, there a {zvuk}, everywhere a {zvuk} {zvuk}.\n" +
+                $"Old MacDonald had a farm, E I E I O.\n");
+        }
+
+    }
+
+
+    class Pig : Animall
+    {
+        public override void OldMcDonaldd(string ime, string zvuk)
+        {
+
+            Console.WriteLine("Old MacDonald had a farm, E I E I O,\n" +
+                $"And on his farm, he had a {ime}, E I E I O. \n" +
+                $"With a {zvuk},{zvuk} here and a {zvuk},{zvuk} there,\n" +
+                $"Here a {zvuk}, there a {zvuk}, everywhere a {zvuk} {zvuk}.\n" +
+                $"Old MacDonald had a farm, E I E I O.\n");
+        }
+    }
+
+
+    class Dog : Animall
+    {
+
+        public override void OldMcDonaldd(string ime, string zvuk)
+        {
+
+            Console.WriteLine("Old MacDonald had a farm, E I E I O,\n" +
+                $"And on his farm, he had a {ime}, E I E I O. \n" +
+                $"With a {zvuk},{zvuk} here and a {zvuk},{zvuk} there,\n" +
+                $"Here a {zvuk}, there a {zvuk}, everywhere a {zvuk} {zvuk}.\n" +
+                $"Old MacDonald had a farm, E I E I O.\n");
+        }
+
+    }
+
+
+    class Cat : Animall
+    {
+
+        public override void OldMcDonaldd(string ime, string zvuk)
+        {
+
+            Console.WriteLine("Old MacDonald had a farm, E I E I O,\n" +
+                $"And on his farm, he had a {ime}, E I E I O. \n" +
+                $"With a {zvuk},{zvuk} here and a {zvuk},{zvuk} there,\n" +
+                $"Here a {zvuk}, there a {zvuk}, everywhere a {zvuk} {zvuk}.\n" +
+                $"Old MacDonald had a farm, E I E I O.\n");
+
+
+        }
+
+    }
+
+
+    class Horse : Animall
+    {
+
+        public override void OldMcDonaldd(string ime, string zvuk)
+        {
+
+            Console.WriteLine("Old MacDonald had a farm, E I E I O,\n" +
+                $"And on his farm, he had a {ime}, E I E I O. \n" +
+                $"With a {zvuk},{zvuk} here and a {zvuk},{zvuk} there,\n" +
+                $"Here a {zvuk}, there a {zvuk}, everywhere a {zvuk} {zvuk}.\n" +
+                $"Old MacDonald had a farm, E I E I O.\n");
+
+
+        }
+
+    }
+
+    class Sheep : Animall
+    {
+
+        public override void OldMcDonaldd(string ime, string zvuk)
+        {
+
+            Console.WriteLine("Old MacDonald had a farm, E I E I O,\n" +
+                $"And on his farm, he had a {ime}, E I E I O. \n" +
+                $"With a {zvuk},{zvuk} here and a {zvuk},{zvuk} there,\n" +
+                $"Here a {zvuk}, there a {zvuk}, everywhere a {zvuk} {zvuk}.\n" +
+                $"Old MacDonald had a farm, E I E I O.\n");
+
+
+        }
+
+    }
+
+    class Turkey : Animall
+    {
+
+        public override void OldMcDonaldd(string ime, string zvuk)
+        {
+
+            Console.WriteLine("Old MacDonald had a farm, E I E I O,\n" +
+                $"And on his farm, he had a {ime}, E I E I O. \n" +
+                $"With a {zvuk},{zvuk} here and a {zvuk},{zvuk} there,\n" +
+                $"Here a {zvuk}, there a {zvuk}, everywhere a {zvuk} {zvuk}.\n" +
+                $"Old MacDonald had a farm, E I E I O.\n");
+
+
+        }
+
+    }
+
+    class Donkey : Animall
+    {
+
+        public override void OldMcDonaldd(string ime, string zvuk)
+        {
+
+            Console.WriteLine("Old MacDonald had a farm, E I E I O,\n" +
+                $"And on his farm, he had a {ime}, E I E I O. \n" +
+                $"With a {zvuk},{zvuk} here and a {zvuk},{zvuk} there,\n" +
+                $"Here a {zvuk}, there a {zvuk}, everywhere a {zvuk} {zvuk}.\n" +
+                $"Old MacDonald had a farm, E I E I O.\n");
+
+
+        }
+
+    }
+
+    class Dolphin : Animall
+    {
+
+        public override void OldMcDonaldd(string ime, string zvuk)
+        {
+
+            Console.WriteLine("Old MacDonald had a farm, E I E I O,\n" +
+                $"And on his farm, he had a {ime}, E I E I O. \n" +
+                $"With a {zvuk},{zvuk} here and a {zvuk},{zvuk} there,\n" +
+                $"Here a {zvuk}, there a {zvuk}, everywhere a {zvuk} {zvuk}.\n" +
+                $"Old MacDonald had a farm, E I E I O.\n");
+
+
+        }
+
+    }
+
+    class Deer : Animall
+    {
+
+        public override void OldMcDonaldd(string ime, string zvuk)
+        {
+
+            Console.WriteLine("Old MacDonald had a farm, E I E I O,\n" +
+                $"And on his farm, he had a {ime}, E I E I O. \n" +
+                $"With a {zvuk},{zvuk} here and a {zvuk},{zvuk} there,\n" +
+                $"Here a {zvuk}, there a {zvuk}, everywhere a {zvuk} {zvuk}.\n" +
+                $"Old MacDonald had a farm, E I E I O.\n");
+
+
+        }
+
+    }
+
+    class Bear : Animall
+    {
+
+        public override void OldMcDonaldd(string ime, string zvuk)
+        {
+
+            Console.WriteLine("Old MacDonald had a farm, E I E I O,\n" +
+                $"And on his farm, he had a {ime}, E I E I O. \n" +
+                $"With a {zvuk},{zvuk} here and a {zvuk},{zvuk} there,\n" +
+                $"Here a {zvuk}, there a {zvuk}, everywhere a {zvuk} {zvuk}.\n" +
+                $"Old MacDonald had a farm, E I E I O.\n");
+
+
+        }
+
+    }
+
+
+
+
+
+
+    internal class Polimorfizam2
+    {
+
+        static void Main(string[] args)
+        {
+
+            Cow c = new Cow();
+            c.OldMcDonaldd("Cow", "Moo");
+
+            Duck d = new Duck();
+            d.OldMcDonaldd("Duck", "Quack");
+
+            Pig g = new Pig();
+            g.OldMcDonaldd("Pig", "Oink");
+
+            Dog a = new Dog();
+            a.OldMcDonaldd("Dog", "Bark");
+
+            Cat t = new Cat();
+            t.OldMcDonaldd("Cat", "Meow");
+
+            Horse h = new Horse();
+            h.OldMcDonaldd("Horse", "Neigh");
+
+            Sheep s = new Sheep();
+            s.OldMcDonaldd("Sheep", "Baa");
+
+            Turkey turkey = new Turkey();
+            turkey.OldMcDonaldd("Turkey","Gobble");
+
+            Donkey donkey = new Donkey();
+            donkey.OldMcDonaldd("Donkey","Hee-haw"); 
+
+            Dolphin dolphin =new Dolphin();
+            dolphin.OldMcDonaldd("Dolphin", "whistle");
+
+            Deer deer= new Deer();
+            deer.OldMcDonaldd("Deer", "bell");
+
+            Bear bear = new Bear();
+            bear.OldMcDonaldd("Bear", "Roar");
+
+
+
+
+
+
+        }
+
+
+
+
+
+    
+
+}
+}
