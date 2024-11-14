8fe338ae4358c2bbcb951de3141a353e96072795
nikolakosturski <nkosturski6@gmail.com>
2024-11-14 17:57:53 +0100

    third commit

--- InterviewTask/Program.cs
+++ InterviewTask/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace InterviewTask
@@ -9,8 +10,8 @@ namespace InterviewTask
 
     public class Animal
     {
-        string ime;
-        string zvuk;
+      private  string ime;
+        private string zvuk;
       public string getIme()
         {
             string Ime = ime;
@@ -31,7 +32,8 @@ namespace InterviewTask
         }
         public void OldMcDonald()
         {   
-            Console.WriteLine($"Old MacDonald had a farm, E I E I O,\nAnd on his farm, he had a {getIme()}, E I E I O." +
+            Console.WriteLine($"Old MacDonald had a farm, E I E I O,\n" +
+                $"And on his farm, he had a {getIme()}, E I E I O." +
                 $"\nWith a {getZvuk()},{getZvuk()} here and a {getZvuk()},{getZvuk()} there," +
                 $"\n Here a {getZvuk()}, there a {getZvuk()}, everywhere a {getZvuk()} {getZvuk()}." +
                 "\nOld MacDonald had a farm, E I E I O.\n");
@@ -42,44 +44,67 @@ namespace InterviewTask
 
 
     {
-
+        
       
 
         static void Main(string[] args)
         {
 
-            Animal animal = new Animal();
-            string zivotno1="cow";
-            animal.setIme( zivotno1);
-            string CowSound = "Moo";
-            animal.setZvuk(CowSound);
-            animal.OldMcDonald();
+            //DATA-DRIVEN APPROACH
 
-            string zivotno2 = "duck";
-            animal.setIme(zivotno2);
-            string duckSound = "Quack";
-            animal.setZvuk(duckSound);
-            animal.OldMcDonald();
 
-            string zivotno3 = "pig";
-            animal.setIme(zivotno3);
-            string pigSound = "Oink";
-            animal.setZvuk(pigSound);
-            animal.OldMcDonald();
+          Animal animal = new Animal();
+          string zivotno1="cow";
+          animal.setIme( zivotno1);
+          string CowSound = "Moo";
+          animal.setZvuk(CowSound);
+          animal.OldMcDonald();
 
-            string zivotno4 = "cat";
-            animal.setIme(zivotno4);
-            string CatSound = "Meow";
-            animal.setZvuk(CatSound);
-            animal.OldMcDonald();
+          string zivotno2 = "duck";
+          animal.setIme(zivotno2);
+          string duckSound = "Quack";
+          animal.setZvuk(duckSound);
+          animal.OldMcDonald();
 
-            string zivotno5 = "dog";
-            animal.setIme(zivotno5);
-            string DogSOund = "Bark";
-            animal.setZvuk(DogSOund);
-            animal.OldMcDonald();
+          string zivotno3 = "pig";
+          animal.setIme(zivotno3);
+          string pigSound = "Oink";
+          animal.setZvuk(pigSound);
+          animal.OldMcDonald();
+
+          string zivotno4 = "cat";
+          animal.setIme(zivotno4);
+          string CatSound = "Meow";
+          animal.setZvuk(CatSound);
+          animal.OldMcDonald();
+
+          string zivotno5 = "dog";
+          animal.setIme(zivotno5);
+          string DogSOund = "Bark";
+          animal.setZvuk(DogSOund);
+          animal.OldMcDonald();
+        
+
+            // KOD ZA TRETO PRASANJE
+
+        Start:
+            Console.WriteLine("Vnesete ime na zivotno: ");
+            string Ime =Console.ReadLine();
+            Console.WriteLine("Vnesete zvuk za vaseto zivotno: ");
+            string zvuk = Console.ReadLine();
             
+            Animal zivotno = new Animal();
 
+            zivotno.setIme(Ime);
+            zivotno.setZvuk(zvuk);
+            zivotno.OldMcDonald();
+
+            if (Ime != string.Empty)
+            {
+                goto Start;
+            }
+
+            
 
         }
     }
