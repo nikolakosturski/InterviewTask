18d1f9d76efabf51931ec456791c23c2ce180f66
nikolakosturski <nkosturski6@gmail.com>
2024-11-14 17:27:06 +0100

    third commit

---
+++ InterviewTask/Polimorfizam.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewTask
+{
+
+    public   class Animall
+    {
+        private string ime;
+        private string Zvuk;
+
+        public string getName()
+        {
+            string name = ime;
+            return ime;
+        }
+        public void setName(string name)
+        {
+            this.ime = name;
+        }
+
+        public string getZvuk()
+        {
+            string Sound = Zvuk;
+            return Sound;
+        }
+
+        public void setZvuk(string zvuk)
+        {
+
+            this.Zvuk = zvuk;
+
+        }
+        public virtual void OldMcDonaldd(string ime, string zvuk) { }
+
+    }
+
+    class Cow : Animall
+    {
+       
+
+
+        public override void OldMcDonaldd(string ime,string zvuk)
+        {
+
+            Console.WriteLine("Old MacDonald had a farm, E I E I O,\n" +
+                $"And on his farm, he had a {ime}, E I E I O. \n"+
+                $"With a {zvuk},{zvuk} here and a {zvuk},{zvuk} there,\n"+
+                $"Here a {zvuk}, there a {zvuk}, everywhere a {zvuk} {zvuk}.\n"+  
+                $"Old MacDonald had a farm, E I E I O.\n");
+        }
+    
+    
+    }
+
+        class Duck : Animall
+    {
+
+        public override void OldMcDonaldd(string ime, string zvuk)
+        {
+
+            Console.WriteLine("Old MacDonald had a farm, E I E I O,\n" +
+                $"And on his farm, he had a {ime}, E I E I O. \n" +
+                $"With a {zvuk},{zvuk} here and a {zvuk},{zvuk} there,\n" +
+                $"Here a {zvuk}, there a {zvuk}, everywhere a {zvuk} {zvuk}.\n" +
+                $"Old MacDonald had a farm, E I E I O.\n");
+        }
+
+    }
+
+
+        class Pig : Animall
+    {
+        public override void OldMcDonaldd(string ime, string zvuk)
+        {
+
+            Console.WriteLine("Old MacDonald had a farm, E I E I O,\n" +
+                $"And on his farm, he had a {ime}, E I E I O. \n" +
+                $"With a {zvuk},{zvuk} here and a {zvuk},{zvuk} there,\n" +
+                $"Here a {zvuk}, there a {zvuk}, everywhere a {zvuk} {zvuk}.\n" +
+                $"Old MacDonald had a farm, E I E I O.\n");
+        }
+    }
+
+
+    class Dog : Animall
+    {
+
+        public override void OldMcDonaldd(string ime, string zvuk)
+        {
+
+            Console.WriteLine("Old MacDonald had a farm, E I E I O,\n" +
+                $"And on his farm, he had a {ime}, E I E I O. \n" +
+                $"With a {zvuk},{zvuk} here and a {zvuk},{zvuk} there,\n" +
+                $"Here a {zvuk}, there a {zvuk}, everywhere a {zvuk} {zvuk}.\n" +
+                $"Old MacDonald had a farm, E I E I O.\n");
+        }
+
+    }
+
+
+    class Cat : Animall
+    {
+
+        public override void OldMcDonaldd(string ime, string zvuk)
+        {
+
+            Console.WriteLine("Old MacDonald had a farm, E I E I O,\n" +
+                $"And on his farm, he had a {ime}, E I E I O. \n" +
+                $"With a {zvuk},{zvuk} here and a {zvuk},{zvuk} there,\n" +
+                $"Here a {zvuk}, there a {zvuk}, everywhere a {zvuk} {zvuk}.\n" +
+                $"Old MacDonald had a farm, E I E I O.\n");
+            
+         
+        }
+
+    }
+
+
+        internal class Polimorfizam
+        {
+
+        static void Main(string[] args)
+        {
+
+            Cow c = new Cow();
+            c.OldMcDonaldd("Cow","Moo");
+
+            Duck d = new Duck();
+            d.OldMcDonaldd("Duck", "Quack");
+
+            Pig g = new Pig();
+            g.OldMcDonaldd("Pig", "Oink");
+
+            Dog a = new Dog();
+            a.OldMcDonaldd("Dog", "Bark");
+
+            Cat t = new Cat();
+            t.OldMcDonaldd("Cat", "Meow");
+
+
+
+
+        }
+
+
+        }
+    }
